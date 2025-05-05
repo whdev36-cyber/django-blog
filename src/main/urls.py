e5a25d0b2b7dd596c3ed164e34a63fe68b99c494
WCyber <whdev36@gmail.com>
2025-05-05 18:59:43 +0500

    feat: add about and yay views with corresponding templates

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), # Home page
+    path('about/', views.about, name='about'), # About page
+    path('yay/', views.yay, name='yay'), # YAY!
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Home page view
 def home(request):
     return render(request, 'home.html') # Render the home.html template
+
+# About page view
+def about(request):
+    return render(request, 'about.html') # Render the about.html template
+
+# YAY!
+def yay(request):
+    return render(request, 'yay.html') # Render the yay.html template
