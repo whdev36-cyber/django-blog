f1bc12deff7cbd2ef27179dfaeb79f4570a59a9a
WCyber <whdev36@gmail.com>
2025-05-06 17:53:37 +0500

    feat: register Tag, Category, and Post models in admin with custom admin classes

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Tag, Category, Post
 
-# Register your models here.
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'views', 'caps', 'created_at']
+    search_fields = ['title', 'author__username', 'category__name']
+    list_filter = ['author', 'category', 'tags', 'created_at']
+    readonly_fields = ['views', 'created_at', 'updated_at']
+    autocomplete_fields = ['author', 'category', 'tags']
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
