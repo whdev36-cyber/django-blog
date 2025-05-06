59cd714ecf54ad2e039206800f8fb7a077b608d9
WCyber <whdev36@gmail.com>
2025-05-06 18:05:13 +0500

    fix: update PostAdmin configuration by commenting out search and autocomplete fields

--- src/main/admin.py
+++ src/main/admin.py
@@ -11,10 +11,10 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'category', 'views', 'caps', 'created_at']
-    search_fields = ['title', 'author__username', 'category__name']
+    # search_fields = ['title', 'author__username', 'category__name']
     list_filter = ['author', 'category', 'tags', 'created_at']
-    readonly_fields = ['views', 'created_at', 'updated_at']
-    autocomplete_fields = ['author', 'category', 'tags']
+    readonly_fields = ['views', 'caps', 'created_at', 'updated_at']
+    # autocomplete_fields = ['author', 'category', 'tags']
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Category, CategoryAdmin)
