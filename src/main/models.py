4e269593d340f7435b4d80ff1ae1d55a832d6f81
WCyber <whdev36@gmail.com>
2025-05-06 17:11:27 +0500

    feat: implement Tag model with slug generation in models.py

--- src/main/models.py
+++ src/main/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
 
-class Tag(models.Model): pass
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Tag, self).save(*args, **kwargs)
+
+
 class Category(models.Model): pass
 class Post(models.Model): pass
