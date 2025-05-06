b2d24d958407ab8fa1c020410609ead675b9d785
WCyber <whdev36@gmail.com>
2025-05-06 17:22:34 +0500

    feat: enhance Category model with detailed docstring and save method

--- src/main/models.py
+++ src/main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 
+# Tag model
 class Tag(models.Model):
     '''
     Tag model to represent a tag in the system.
@@ -11,11 +12,33 @@ class Tag(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
-        super(Tag, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 
-class Category(models.Model): pass
+# Category model
+class Category(models.Model):
+    '''
+    Category model used to group related posts or items.
+    Each category has a unique name and a slug for SEO-friendly URLs.
+    '''
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
 class Post(models.Model): pass
