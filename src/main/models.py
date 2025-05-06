1963264f22a20ff775ac661dbc94b9010ee9dc55
WCyber <whdev36@gmail.com>
2025-05-06 17:17:21 +0500

    feat: update Tag model to ensure slug uniqueness and improve string representation

--- src/main/models.py
+++ src/main/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from django.utils.text import slugify
 
 class Tag(models.Model):
+    '''
+    Tag model to represent a tag in the system.
+    '''
     name = models.CharField(max_length=64)
-    slug = models.SlugField(max_length=64, unique=True, blank=True, null=True)
+    slug = models.SlugField(max_length=64, unique=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         super(Tag, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model): pass
