deb2c8342fd44eab25c0410e37a28dac01806491
WCyber <whdev36@gmail.com>
2025-05-09 14:40:11 +0500

    refactor: clean up views.py by removing unnecessary comments and whitespace

--- src/main/views.py
+++ src/main/views.py
@@ -8,11 +8,9 @@ def home(request):
     featured_post = Post.objects.filter(is_public=True, is_featured=True).order_by('-created_at').first()
     posts = Post.objects.filter(is_public=True).exclude(id=featured_post.id if featured_post else None).order_by('-created_at')
 
-    # Paginator
     paginator = Paginator(posts, 6)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-
 
     return render(request, 'home.html', {
         'featured_post': featured_post,
@@ -24,6 +22,7 @@ def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'post_detail.html', {'post': post})
 
+
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     return HttpResponse(category.name)
@@ -32,10 +31,10 @@ def category_detail(request, slug):
 def search(request):
     return HttpResponse('search')
 
-# About page view
-def about(request):
-    return render(request, 'about.html') # Render the about.html template
 
-# YAY!
+def about(request):
+    return render(request, 'about.html')
+
+
 def yay(request):
-    return render(request, 'yay.html') # Render the yay.html template
+    return render(request, 'yay.html')
